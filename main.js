a8dd4f4fcc0062456d8f17e46667ff7db3db40cb
jamayccaaa <jam10189@nyu.edu>
2025-09-29 23:52:31 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
